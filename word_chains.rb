d8e5cd29d64551cd7b2a1e5e9f3ee2146cfb2517
alaughlin <adamlaughlin@outlook.com>
2014-07-12 12:20:55 -0400

    added build_path method, not working yet though

--- word_chains.rb
+++ word_chains.rb
@@ -31,27 +31,51 @@ class WordChainer
     same_length
   end
 
-  def run(source, target)
-    @current_words = [source]
-    @all_seen_words = [source]
-
-    until @current_words.empty?
-      new_current_words = []
-      @current_words.each do |cur_word|
-        adjacent_words(cur_word).each do |adj_word|
-          unless @all_seen_words.include?(adj_word)
-            new_current_words << adj_word
-            @all_seen_words << adj_word
-          end
+  def explore_current_words
+    new_current_words = []
+    @current_words.each do |cur_word|
+      adjacent_words(cur_word).each do |adj_word|
+        unless @all_seen_words.include?(adj_word)
+          new_current_words << adj_word
+          @all_seen_words[adj_word] = cur_word
         end
       end
+    end
 
-      p new_current_words
+    new_current_words.each do |word|
+      puts "#{word} came from #{@all_seen_words[word]}"
+    end
+
+    new_current_words
+  end
+
+  def build_path(target)
+    path = []
+
+    cur_target = target
+    until cur_target == "duck"
+      path.unshift(target)
+      cur_target = @all_seen_words[target]
+    end
+
+    p path
+  end
+
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = Hash.new { |hash, key| hash[key] = nil }
+    @all_seen_words[source]
+
+    until @current_words.empty? || @all_seen_words.include?(target)
+      new_current_words = explore_current_words
+      
       @current_words = new_current_words
     end
+
+    build_path(target)
 
   end
 end
 
 wc = WordChainer.new("dictionary.txt")
-wc.run("duck", "ruby")
+wc.run("duck", "sick")
