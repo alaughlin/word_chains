5645f2f4c1a6c4e9ce5cc8dda68137c381515888
alaughlin <adamlaughlin@outlook.com>
2014-07-13 21:38:07 -0400

    rewrote word_chains.rb, something was wrong. working now.

--- word_chains.rb
+++ word_chains.rb
@@ -1,61 +1,58 @@
 require 'set'
 
 class WordChainer
-  def initialize(dictionary)
-    @dictionary = File.readlines(dictionary).map(&:chomp).to_set
+  def initialize(dict_file)
+    @dictionary = File.readlines(dict_file).map(&:chomp).to_set
   end
 
   def adjacent_words(word)
-    same_length = same_length_words(word)
+    same_length_words = get_same_length(word)
 
     res = []
-    
-    same_length.each do |dict_word|
+
+    same_length_words.each do |same_length_word|
       count = 0
-      (word.length).times do |i|
-        count += 1 if dict_word[i] != word[i]
+
+      (same_length_word.length).times do |i|
+        count += 1 if same_length_word[i] != word[i]
       end
 
-      res << dict_word if count == 1
+      res << same_length_word if count == 1
     end
 
     res
   end
 
-  def same_length_words(word)
-    same_length = Set.new
-    @dictionary.each do |dict_word| 
-      same_length << dict_word if dict_word.length == word.length
-    end
-
-    same_length
+  def get_same_length(word)
+    @dictionary.select { |dict_word| word.length == dict_word.length }
   end
 
   def explore_current_words
     new_current_words = []
-    @current_words.each do |cur_word|
-      adjacent_words(cur_word).each do |adj_word|
+
+    @current_words.each do |current_word|
+      adjacent_words(current_word).each do |adj_word|
         unless @all_seen_words.include?(adj_word)
           new_current_words << adj_word
-          @all_seen_words[adj_word] = cur_word
+          @all_seen_words[adj_word] = current_word
         end
       end
     end
 
-    new_current_words.each do |word|
-      puts "#{word} came from #{@all_seen_words[word]}"
-    end
+    @current_words = new_current_words
 
-    new_current_words
+    new_current_words.each do |current_word|
+      puts "#{current_word} came from #{@all_seen_words[current_word]}"
+    end
   end
 
   def build_path(target)
     path = []
+    current_target = target
 
-    cur_target = target
-    until cur_target == "duck"
-      path.unshift(target)
-      cur_target = @all_seen_words[target]
+    until current_target.nil?
+      path << current_target
+      current_target = @all_seen_words[current_target]
     end
 
     p path
@@ -63,19 +60,15 @@ class WordChainer
 
   def run(source, target)
     @current_words = [source]
-    @all_seen_words = Hash.new { |hash, key| hash[key] = nil }
-    @all_seen_words[source]
+    @all_seen_words = { source => nil }
 
-    until @current_words.empty? || @all_seen_words.include?(target)
-      new_current_words = explore_current_words
-      
-      @current_words = new_current_words
+    until @all_seen_words.include?(target)
+      explore_current_words
     end
 
     build_path(target)
-
   end
 end
 
 wc = WordChainer.new("dictionary.txt")
-wc.run("duck", "sick")
+wc.run("duck", "ruby")
